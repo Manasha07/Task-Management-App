3ab665a0092dd983a72624a9d4ed3dbe58731a73
KishoreRam07 <engineeringpostbox@gmail.com>
2025-04-13 00:04:04 +0530

    update on project form 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { ProjectListComponent } from './project-list/project-list.component';
+import { ProjectFormComponent } from './project-form/project-form.component'; // import it
 
 export const routes: Routes = [
-    { path: 'projects', component: ProjectListComponent },
-    { path: '', redirectTo: 'projects', pathMatch: 'full' }
+  { path: '', component: ProjectListComponent },
+  { path: 'projects/add', component: ProjectFormComponent }, // this matches your button!
 ];
